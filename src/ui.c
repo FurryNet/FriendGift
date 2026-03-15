a3cc39c8b8fb9167af5edc58de95c636042f8a3f
zhiyan114 <zhiyan114@furries.dev>
2026-03-15 16:51:14 -0400

    YEE HAW

--- src/ui.c
+++ src/ui.c
@@ -81,9 +81,10 @@ void init_result_screen(rps_outcome outcome, rps_choice player_choice, rps_choic
 
     // If win, display the gift card code, otherwise prompt to try again
     if (outcome == WIN) {
-        char code_str[17]; // 16 horizonal max display + null term
-        snprintf(code_str, sizeof(code_str), "Code: %s", gift_card_code);
-        display_write_page(code_str, 2, 1);
+        // char code_str[17]; // 16 horizonal max display + null term
+        // snprintf(code_str, sizeof(code_str), "Code: %s", gift_card_code);
+        display_write_page("Gift Card Code:", 2, 1);
+        display_write_page("{Codes Here}", 3, 1);
     } else {
         display_write_page("Press START to try again!", 2, 1);
     }
