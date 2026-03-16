75851c9d3f2b263360066a6ba57c27ea996e731b
GameBoyAdvSP <90002537+Meowcaroni@users.noreply.github.com>
2026-03-15 21:47:43 -0400

    Fix init_result_screen header parameters

--- include/ui.h
+++ include/ui.h
@@ -3,4 +3,4 @@
 void init_main_menu();
 void init_game_screen();
 void update_game_screen(rps_choice player_choice);
-void init_result_screen(rps_outcome outcome, rps_choice player_choice, rps_choice cpu_choice, char* gift_card_code);
+void init_result_screen(rps_outcome outcome, rps_choice player_choice, rps_choice cpu_choice);
